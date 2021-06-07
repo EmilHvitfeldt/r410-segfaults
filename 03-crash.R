f3d5d5886647b3abee06ed671e24ffbd0a403ca6
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2021-06-06 23:27:22 -0700

    comments

--- 03-crash.R
+++ 03-crash.R
@@ -1,3 +1,4 @@
+# Setup
 let <- expand.grid(letters, letters, letters, letters)
 
 tokens <- paste0(let[[1]], let[[2]], let[[3]], let[[4]])
@@ -7,8 +8,10 @@ other_tokens <- c("אל")
 tokens <- c(other_tokens, tokens)
 
 set.seed(1234)
+# Does not error if there isn't enough NAs
 res <- sample(c(rep(NA, 1000000), tokens))
 
 vocabulary <- setNames(seq_along(tokens), tokens)[seq_len(1000)]
 
+# Error
 new <- vocabulary[res]
